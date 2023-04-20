6ad12bbee0f44702eaddac001dae545a549fb6a5
Ernest2104 <villagra9@gmail.com>
2023-04-20 16:13:12 -0300

    commit 20-04-23

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -5,30 +5,88 @@ const axios = require('axios');
 
 // pokemones de la API
 const getApiInfo = async () => {
-    const arrayPokemons = []   
-    for (let id = 1; id <= 40; id++) {    
-        arrayPokemons.push(axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`));// arrayPokemons -> array de promesas         
-    }
+    // const arrayPokemons = []   
+    // for (let id = 1; id <= 40; id++) {    
+    //     arrayPokemons.push(axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`));// arrayPokemons -> array de promesas         
+    // }
 
-    const apiInfo = await Promise.all(arrayPokemons)// Promise -> retorna una promesa cuando todas las promesas del argumento concluyeron
-    .then(pokemon => {
-        const apiPokes = pokemon.map(poke => {
-            return {
-                id: poke.data.id,
-                name: poke.data.name.charAt(0).toUpperCase() + poke.data.name.slice(1),
-                hp: poke.data.stats[0].base_stat,
-                attack: poke.data.stats[1].base_stat,
-                defense: poke.data.stats[2].base_stat,
-                speed: poke.data.stats[5].base_stat,
-                weight: poke.data.weight,
-                height: poke.data.height,
-                sprite: poke.data.sprites.other.dream_world.front_default,
-                types: poke.data.types.map(t => t.type.name.charAt(0).toUpperCase() + t.type.name.slice(1))
-            }
-        })
-        return apiPokes;
-    })            
-    return apiInfo;
+    // const apiInfo = await Promise.all(arrayPokemons)// Promise -> retorna una promesa cuando todas las promesas del argumento concluyeron
+    // .then(pokemon => {
+    //     const apiPokes = pokemon.map(poke => {
+    //         return {
+    //             id: poke.data.id,
+    //             name: poke.data.name.charAt(0).toUpperCase() + poke.data.name.slice(1),
+    //             hp: poke.data.stats[0].base_stat,
+    //             attack: poke.data.stats[1].base_stat,
+    //             defense: poke.data.stats[2].base_stat,
+    //             speed: poke.data.stats[5].base_stat,
+    //             weight: poke.data.weight,
+    //             height: poke.data.height,
+    //             sprite: poke.data.sprites.other.home.front_default,
+    //             types: poke.data.types.map(t => t.type.name.charAt(0).toUpperCase() + t.type.name.slice(1))
+    //         }
+    //     })
+    //     return apiPokes;
+    // })            
+    // return apiInfo;
+
+    // const resp = await axios
+    //   .get("https://pokeapi.co/api/v2/pokemon?limit=40")
+    //   .then((data) => {
+    //     return data.data.pokemons.datas;
+    //   })
+    //   .then((data) => {
+    //     return Promise.all(data.map((res) => axios.get(res.url))); // ENTRAR A CADA ELEMENTO Y HACERLE UN GET A SU URL
+    //   })
+    //   .then((data) => {
+    //     return data.map((res) => res.data); // pokemons.dataADO FINAL DE CADA POKEMON CON TODOS SUS DATOS, SE GUARDAN EN RESP.
+    //   });
+    // let arrayPoke = resp.map((pokemons.data) => {  //DENTRO DE UN ARRAY ME TRAIGO TODAS LAS PROPIEDADES QUE QUIERO DE CADA POKEMON.
+    //   return {
+    //     id: pokemons.data.id,
+    //     name: pokemons.data.name.charAt(0).toUpperCase() + poke.data.name.slice(1),
+    //     hp: pokemons.data.stats[0].base_stat,
+    //     attack: pokemons.data.stats[1].base_stat,
+    //     defense: pokemons.data.stats[2].base_stat,
+    //     speed: pokemons.data.stats[5].base_stat,
+    //     weight: pokemons.data.weight,
+    //     height: pokemons.data.height,
+    //     sprite: pokemons.data.sprites.other.home.front_default,
+    //     types: pokemons.data.types.map(t => t.type.name.charAt(0).toUpperCase() + t.type.name.slice(1))
+    //     // id: pokemons.data.id,
+    //     // name: pokemons.data.name,
+    //     // types: pokemons.data.types.map((t) => t.type.name), //lOS TIPOS ESTAN EN SU PROPIEDAD NAME
+    //     // image: pokemons.data.sprites.front_default,
+    //     // life: pokemons.data.stats[0].base_stat,
+    //     // attack: pokemons.data.stats[1].base_stat,
+    //     // defense: pokemons.data.stats[2].base_stat,
+    //     // speed: pokemons.data.stats[3].base_stat,
+    //     // height: pokemons.data.height,
+    //     // weight: pokemons.data.weight,
+    //   };
+    // });
+    // return arrayPoke;
+
+    const apiPokemons = [];
+    const pokemonRequest = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=40");
+    const urlPokemonSubrequest = pokemonRequest.data.pokemons.datas.map((pokemon) => pokemon.url);
+    await axios.all(urlPokemonSubrequest.map((urlPokemonSubrequest) => axios.get(urlPokemonSubrequest))).then(
+      (pokemons) => {
+        pokemons.map((pokemons) => apiPokemons.push({
+            id: pokemons.data.id,
+            name: pokemons.data.name.charAt(0).toUpperCase() + pokemons.data.name.slice(1),
+            hp: pokemons.data.stats[0].base_stat,
+            attack: pokemons.data.stats[1].base_stat,
+            defense: pokemons.data.stats[2].base_stat,
+            speed: pokemons.data.stats[5].base_stat,
+            weight: pokemons.data.weight,
+            height: pokemons.data.height,
+            sprite: pokemons.data.sprites.other.home.front_default,
+            types: pokemons.data.types.map(t => t.type.name.charAt(0).toUpperCase() + t.type.name.slice(1))
+        }));
+      },
+    );
+    return apiPokemons;
 };
 
 // pokemones de la base de datos
